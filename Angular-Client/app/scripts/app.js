88cafb49b05848a0df317f7094948eb62b6b5f3e
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-04-16 18:29:19 -0400

    Preloader added

--- Angular-Client/app/scripts/app.js
+++ Angular-Client/app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angularUtils.directives.dirPagination'
   ])
   .config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('')
---
+++ Angular-Client/app/scripts/js/materializeCustom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {  
+  $('.button-collapse').sideNav({
+    menuWidth: 280, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  $('.preloader-background').delay(1700).fadeOut('slow');
+
+  $('.preloader-wrapper')
+    .delay(1700)
+    .fadeOut();
+});
